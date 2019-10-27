15754e59f5c4985a987ab5ca0a45fe9670fffe28
Yeongmin Kim <oinochoe@naver.com>
2019-10-28 02:11:16 +0900

    swiper 버전 완성.. 가장 비슷하다

--- src/landing.js
+++ src/landing.js
@@ -1,11 +1,40 @@
 // index.js
 import _ from "lodash";
 import "../scss/style.scss";
-import skrollr from "skrollr";
+import Swiper from "swiper";
 (function() {
-  var s = skrollr.init({
-    forceHeight: true,
-    smoothScroolling: true,
-    smoothScrollingDuration: 600,
-  });
-})();
+  "use strict";
+
+  const breakpoint = window.matchMedia("(max-width:1024px)");
+
+  let mySwiper;
+
+  const breakpointChecker = function() {
+    if (breakpoint.matches === true) {
+      if (mySwiper !== undefined) mySwiper.destroy(true, true);
+
+      return;
+    } else if (breakpoint.matches === false) {
+      return enableSwiper();
+    }
+  };
+  const enableSwiper = function() {
+    mySwiper = new Swiper(".swiper-container", {
+      scrollbar: ".swiper-scrollbar",
+      scrollbarHide: true,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      grabCursor: true,
+      freeModeMinimumVelocity: 0.3,
+      mousewheel: { invert: false },
+      speed: 1500,
+      parallax: true,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+    });
+  };
+  breakpoint.addListener(breakpointChecker);
+  breakpointChecker();
+})(); /* IIFE end */
